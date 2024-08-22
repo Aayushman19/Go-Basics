a20b802be9db6a0b4c26ef90ae36e06dc33e2375
Aayushman Mishra <aayushmanmishra6@gmail.com>
2024-08-22 11:47:02 +0530

    collector working

--- scraper.go
+++ scraper.go
@@ -10,5 +10,22 @@ func main(){
 	args := os.Args
 	url := args[1]
 	collector := colly.NewCollector()
-	
+
+	collector.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	collector.OnResponse(func(r *colly.Response) {
+		fmt.Println("Got a response from", r.Request.URL)
+	})
+
+	collector.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Error occured ", err)
+	})
+
+	collector.OnHTML("h1", func(h *colly.HTMLElement) {
+		fmt.Println(h.DOM.Html())
+	})
+
+	collector.Visit(url)
 }
